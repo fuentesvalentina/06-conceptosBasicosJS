c0bf3e83fd01cdfa10fb61bc4c9f3f79f5d03c5e
Valentina Fuentes <fuentesvalentina74@gmail.com>
2023-10-25 17:21:07 -0300

    agregamos un document.write y un alert

---
+++ js/01-variables.js
@@ -0,0 +1,22 @@
+//esto es un comentario de una unica linea
+
+/*esto es un comentario
+de multiples lineas*/
+
+//delcarar una variable (var - let - const) var no es usaba
+let anioActual = 2023;
+const universe = "https://universe.rollingcodeschool.com/";
+
+//mostrar mensajes
+console.log(anioActual);
+console.log("anioActual");
+console.log("Año actual: " + anioActual);
+console.log(universe);
+
+// modificar el contenido de una variable
+anioActual = 2024;
+console.log(anioActual);
+
+document.write('<p class:"parrafoDestacado">esto es una prueba de documento.write</p>')
+
+alert('Hola Mundo')
