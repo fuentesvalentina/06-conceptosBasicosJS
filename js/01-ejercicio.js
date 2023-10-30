96baba844ebe5385d4b5970c2f75ca8a66ac6db2
Valentina Fuentes <fuentesvalentina74@gmail.com>
2023-10-29 23:07:20 -0300

    Realizo los primeros 5 ejercicios de JS

---
+++ js/01-ejercicio.js
@@ -0,0 +1,18 @@
+alert('Un mensaje');
+
+document.write('Hello World');
+
+document.write('<br>');
+const nombreUsuario = prompt('Ingrese su nombre');
+document.write('Bienvenido ' +nombreUsuario);
+
+document.write('<br>');
+
+document.write('la suma de 5 + 3 = 8')
+
+document.write('<br>');
+
+let numero1 = parseInt(prompt('Ingrese un número'));
+let numero2 = parseInt(prompt('Ingrese otro numero'));
+document.write('El resultado de la suma es igual a '+(numero1+numero2));
+
